895c40ba3b687f787bf07e7f882e0f275c8e44f5
andrewronald <33008952+andrewronald@users.noreply.github.com>
2018-10-10 18:41:25 -0700

    Add files via upload

---
+++ assignment2.lua
@@ -0,0 +1,23 @@
+function ends_in_3(num)
+	local lastDigit = (num % 10)
+	if lastDigit == 3 then return true
+	else return false
+	end
+end
+function is_prime(num)
+	for i = 2, num^(1/2) do
+		if(num%i) == 0 then
+			return false
+		end
+	end
+	return true
+end
+is_prime(991)
+function primeNumEndsIn3(n)
+	for i = 1, n do
+		if(ends_in_3(i) == true and is_prime(i) == true) then
+			print(i)
+		end
+	end
+end
+primeNumEndsIn3(1000)
